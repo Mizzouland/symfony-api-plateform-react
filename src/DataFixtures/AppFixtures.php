c35c7b5bc696aab40905abd547c1dab4bc0222f3
Mizzou <stephanepraud@hotmail.fr>
2019-10-09 22:15:49 +0200

    video 17 - 18 - 19 - 20 ajout entity user et liaison de entity user sur les customers avec Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,12 +4,23 @@ namespace App\DataFixtures;
 
 use App\Entity\Customer;
 use App\Entity\Invoice;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /** @var UserPasswordEncoderInterface  */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
@@ -17,26 +28,53 @@ class AppFixtures extends Fixture
 
         $faker = Factory::create('fr_FR');
 
-        for ($c = 0; $c < 30; $c++) {
-            $customer = new Customer();
-            $customer->setFirstname($faker->firstName())
-                ->setLastname($faker->lastName)
-                ->setCompany($faker->company)
-                ->setEmail($faker->email);
+        for ($u = 0; $u < 10; $u++) {
+            $user = new User();
 
-            // on demande à doctrine de se préparer a faire percister mon customer
-            $manager->persist($customer);
+            // a chaque fois que l'on parle d'un nouvel utilisateur le chrono des facture recommence à 1
+            $chrono = 1;
+            $hash = $this->encoder->encodePassword($user, 'password');
 
-            for ($i = 0; $i < mt_rand(3,10); $i ++) {
-                $invoice = new Invoice();
-                $invoice->setAmount($faker->randomFloat(2,250,5000))
-                    ->setSentAt($faker->dateTimeBetween('- 6 months'))
-                    ->setStatus($faker->randomElement(['SENT', 'PAID','CANCELLED']))
-                    ->setCustomer($customer);
+            $user->setFirstname($faker->lastName)
+                    ->setLastname($faker->lastName)
+                    ->setEmail($faker->email)
+                    ->setPassword($hash);
 
-                $manager->persist($invoice);
+            $manager->persist($user);
+
+            for ($c = 0; $c < mt_rand(5, 20); $c++) {
+                $customer = new Customer();
+                $customer->setFirstname($faker->firstName())
+                    ->setLastname($faker->lastName)
+                    ->setCompany($faker->company)
+                    ->setEmail($faker->email)
+                    ->setUser($user);
+
+                // on demande à doctrine de se préparer a faire percister mon customer
+                $manager->persist($customer);
+
+                for ($i = 0; $i < mt_rand(3,10); $i ++) {
+                    $invoice = new Invoice();
+                    $invoice->setAmount($faker->randomFloat(2,250,5000))
+                        ->setSentAt($faker->dateTimeBetween('- 6 months'))
+                        ->setStatus($faker->randomElement(['SENT', 'PAID','CANCELLED']))
+                        ->setCustomer($customer)
+                        ->setChrono($chrono);
+
+                    $chrono++;
+
+                    $manager->persist($invoice);
+
+
+                }
+
             }
+
+
+
         }
+
+
 
         $manager->flush();
     }
