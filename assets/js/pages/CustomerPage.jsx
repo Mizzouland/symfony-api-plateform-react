f0a392c123aef620a822dd72e145168270063a10
Mizzou <stephanepraud@hotmail.fr>
2020-03-27 11:44:01 +0100

    Feat : 1210 PARTIE 5 - GESTION DE LA SOUMISSION DU FORMULAIRE DE MODIFICATION D'UN CLIENT

--- assets/js/pages/CustomerPage.jsx
+++ assets/js/pages/CustomerPage.jsx
@@ -1,9 +1,42 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 import Field from "../components/forms/Field";
 import {Link} from "react-router-dom";
 import axios from "axios";
 
 const CustomerPage = (props) => {
+
+    console.log(props);
+    // ON SORT L'IDENTIFIANT DES PARAMS AFIN DE DETERMINER SI NOUS SOMMES EN MODE EDITION OU CREATION
+    const {id = "create"} = props.match.params;
+
+
+    // ON VA ENSUITE CRER UN STATE QUI VA PERMETTRE DE DISCRIMER SI JE SUIS EN MODE EDIT OU CREATE
+    // ET PAR DEFAUT NOUS NE SOMMES PAS EN TRAIN D'EDITER
+    const [editing, setEditing] = useState(false);
+    // je vais recevoir une réponse que je vais extraire de response.data
+    const fetchCustomer = async id => {
+
+        try {
+            const data = await axios
+                .get("http://localhost:8000/api/customers/" + id)
+                .then(response => response.data);
+
+            const {firstname, lastname, email, company} = data;
+            setCustomer({firstname, lastname, email, company});
+        }catch (error) {
+            console.log(error.response);
+        }
+    };
+
+    // ON effectue un useEffect a chaque fois que la variable id va changer
+    useEffect(() => {
+        if (id !== 'create') {
+            setEditing(true);
+            fetchCustomer(id);
+        }
+    }, [id]);
+
+
 
     // 1 - si on donne un state et une value il faut automatiquement donner une propriété onChange
     const [customer, setCustomer] = useState({
@@ -41,9 +74,16 @@ const CustomerPage = (props) => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            // je vais attendre la réponse de axios avec une requete en post
-            // et quant on envoie une requete en post nous ajoutons un objet qui sera customer
-           const response =  await axios.post('http://127.0.0.1:8000/api/customers', customer);
+            if (editing) {
+                const response =  await axios.put('http://127.0.0.1:8000/api/customers/'+id, customer);
+                console.log(response.data);
+            } else {
+                // je vais attendre la réponse de axios avec une requete en post
+                // et quant on envoie une requete en post nous ajoutons un objet qui sera customer
+                const response =  await axios.post('http://127.0.0.1:8000/api/customers', customer);
+            }
+
+
            setErrors({});
         } catch (error) {
             const violations = error.response.data.violations;
@@ -57,9 +97,10 @@ const CustomerPage = (props) => {
         }
     };
 
+    // si je ne suis pas en mode édition alors je suis en création
     return (
         <>
-            <h1>Création d'un client</h1>
+            {!editing && <h1>Création d'un client</h1> || <h1>Edition d'un client</h1>}
 
             <form onSubmit={handleSubmit}>
                 <Field name="lastname" label="Nom de famille" placeholder="Nom de famille du client"
